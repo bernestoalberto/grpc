77dcf640f8cfee8f0f5751c69c8f6cd103b4e915
Ernesto <bernestoalberto@gmail.com>
2018-05-22 14:27:35 -0400

    GRPC first

---
+++ server.js
@@ -0,0 +1,54 @@
+var grpc = require('grpc');
+var booksProto = grpc.load('books.proto');
+// In-memory array of book objects
+var books = [
+  { id: 123, title: 'A Tale of Two Cities', author: 'Charles Dickens' }
+];
+// add the events package
+var events = require('events');
+// add the bookStream global variable
+var bookStream = new events.EventEmitter();
+
+var server = new grpc.Server();
+server.addService(booksProto.books.BookService.service, {
+	list: function(call, callback){
+	callback(null, books);
+	},
+	insert: function(call, callback){
+		var book = call.request;
+		  bookStream.emit('new_book', book);
+		books.push(book);
+		callback(null, {});
+	},
+	   get: function(call, callback) {
+        for (var i = 0; i < books.length; i++)
+            if (books[i].id == call.request.id)
+                return callback(null, books[i]);
+        callback({
+            code: grpc.status.NOT_FOUND,
+            details: 'Not found'
+        });
+    },
+	   delete: function(call, callback) {
+        for (var i = 0; i < books.length; i++) {
+            if (books[i].id == call.request.id) {
+                books.splice(i, 1);
+                return callback(null, {});
+            }
+        }
+        callback({
+            code: grpc.status.NOT_FOUND,
+            details: 'Not found'
+        });
+    },
+	    watch: function(stream) {
+        bookStream.on('new_book', function(book){
+            stream.write(book);
+        });
+    }
+});
+
+server.bind('0.0.0.0:50051',  grpc.ServerCredentials.createInsecure());
+console.log('Server running at http://0.0.0.0:50051');
+server.start();
+
